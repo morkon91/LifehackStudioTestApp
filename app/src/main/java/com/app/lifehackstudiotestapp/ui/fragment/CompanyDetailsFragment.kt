bf7c5e552f0e6af36d3af378a256847c4fd793aa
Konstantin Mordokhovich <morkon91@gmail.com>
2021-03-30 11:43:38 +0700

    2

--- app/src/main/java/com/app/lifehackstudiotestapp/ui/fragment/CompanyDetailsFragment.kt
+++ app/src/main/java/com/app/lifehackstudiotestapp/ui/fragment/CompanyDetailsFragment.kt
@@ -1,9 +1,12 @@
 package com.app.lifehackstudiotestapp.ui.fragment
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.widget.AlertDialogLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -14,7 +17,11 @@ import com.app.lifehackstudiotestapp.ui.base.BaseFragment
 import com.app.lifehackstudiotestapp.utils.resolveVisibility
 import com.app.lifehackstudiotestapp.viewModel.CompanyDetailsViewModel
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import javax.inject.Inject
 
 class CompanyDetailsFragment :
@@ -79,21 +86,60 @@ class CompanyDetailsFragment :
     }
 
     private fun showCompanyDetails(result: CompanyDetailsViewModel.GetCompanyDetailsResult.Success) {
-        binding.companyFragmentLinearLayout.visibility = View.VISIBLE
         binding.companyNameTextView.text = result.companyData[0].name
         binding.descriptionTextView.text = result.companyData[0].description
 
         Glide.with(binding.root.context)
-            .asBitmap()
             .load("https://lifehack.studio/test_task/${result.companyData[0].img}")
-            .placeholder(R.drawable.ic_baseline_error_outline_24)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.companyDetailsImage.setImageResource(R.drawable.ic_baseline_error_outline_24)
+                    return true
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.companyDetailsImage.setImageDrawable(resource)
+                    return true
+                }
+
+            })
             .apply(RequestOptions().centerCrop())
             .into(binding.companyDetailsImage)
+
+        binding.locationLinearLayout.resolveVisibility(
+            !result.companyData[0].lat.equals(0.0F) &&
+                    !result.companyData[0].lon.equals(0.0F)
+        )
+
+        binding.companyLocationTextView.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_VIEW
+            intent.data = Uri.parse("geo:${result.companyData[0].lat},${result.companyData[0].lon}")
+            try {
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                showToast("Нет подходящего приложения для просмотра геопозиции")
+            }
+
+        }
 
         binding.phoneLinearLayout.resolveVisibility(result.companyData[0].phone != "")
         binding.siteLinearLayout.resolveVisibility(result.companyData[0].www != "")
         binding.companyPhoneTextView.text = result.companyData[0].phone
         binding.companySiteTextView.text = result.companyData[0].www
+
+        binding.companyFragmentLinearLayout.visibility = View.VISIBLE
     }
 
     private fun showAlertDialog(result: CompanyDetailsViewModel.GetCompanyDetailsResult.Error) {
--- app/src/main/java/com/app/lifehackstudiotestapp/ui/item/CompanyItem.kt
+++ app/src/main/java/com/app/lifehackstudiotestapp/ui/item/CompanyItem.kt
@@ -11,8 +11,12 @@ import com.app.lifehackstudiotestapp.R
 import com.app.lifehackstudiotestapp.databinding.ItemCompanyBinding
 import com.app.lifehackstudiotestapp.model.entity.CompanyBriefData
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.request.transition.Transition
 import com.mikepenz.fastadapter.binding.AbstractBindingItem
 
@@ -26,9 +30,30 @@ class CompanyItem(var company: CompanyBriefData) : AbstractBindingItem<ItemCompa
         binding.companyNameItemTextView.text = company.name
 
         Glide.with(binding.root.context)
-            .asBitmap()
             .load("https://lifehack.studio/test_task/${company.img}")
-            .placeholder(R.drawable.ic_baseline_error_outline_24)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.imageCompany.setImageResource(R.drawable.ic_baseline_error_outline_24)
+                    return true
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.imageCompany.setImageDrawable(resource)
+                    return true
+                }
+
+            })
             .apply(RequestOptions().centerCrop())
             .into(binding.imageCompany)
     }
